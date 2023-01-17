b73611a444d7c55babcb3a78d533e103e19eec74
yunimm <yuu9916@gmail.com>
2023-01-17 23:28:49 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <h2 className="test">dkjfklsjfklds</h2>
+        </div>
+    );
+}
+
+export default App;
